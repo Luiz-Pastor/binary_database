253863cb3db5ea028eae6aa1afd41933ba798efa
Luiz-Pastor <luigpacr5@gmail.com>
2023-11-16 22:42:52 +0100

    Added file with the main structs

---
+++ structs.h
@@ -0,0 +1,24 @@
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+#include <stdio.h>
+# include <stdlib.h>
+
+# define ISBN_LENGTH 16
+# define MAX_LENGTH 128
+
+typedef struct{
+    int         key;    /* Book bookID */
+    long int    offset; /*Book is stored in disk at this position */
+    size_t      size;   /* Book record size. This is a redundant field that helps in the implementation */
+} indexbook;
+
+
+typedef struct{
+    indexbook   index;
+    char        ISBN[ISBN_LENGTH];
+    char        *title;        /* Tamaño variable, máximo de `MAX_LENGTH`*/
+    char        *printedBy;    /* Tamaño variable, máximo de `MAX_LENGTH`*/
+} Element;
+
+#endif
