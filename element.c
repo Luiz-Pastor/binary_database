0d4674bff90e502a187a0797f692dc3c7356b7c5
Luiz-Pastor <luigpacr5@gmail.com>
2023-11-18 13:56:59 +0100

    Added functions to create, delete and print an element

---
+++ element.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "element.h"
+
+Element *createElement()
+{
+	int		i;
+	Element	*element;
+
+	element = (Element*) malloc(sizeof(Element));
+	if (!element)
+		return (NULL);
+
+	element->title = NULL;
+	element->printedBy = NULL;
+
+	element->index.key = 0;
+	element->index.offset = 0;
+	element->index.size = 0;
+
+	for (i = 0; i < ISBN_LENGTH + 1; i++)
+		element->isbn[i] = '\0';
+
+	return (element);
+}
+
+
+void	deleteElement(Element *element)
+{
+	if (!element)
+		return;
+
+	if (element->title)
+		free(element->title);
+	if (element->printedBy)
+		free(element->printedBy);
+	free(element);
+}
+
+void	printElement(Element *element)
+{
+	printf("Offset: %ld\n", element->index.offset);
+    printf("Tamano del bloque: %ld\n", element->index.size);
+    printf("\tBookID:\t\t%d\n", element->index.key);
+    printf("\tIDBN:\t\t%s\n", element->isbn);
+    printf("\tTitle:\t\t%s\n", element->title);
+    printf("\tEditorial:\t%s\n", element->printedBy);
+}
---
+++ element.h
@@ -0,0 +1,10 @@
+#ifndef ELEMENT_H
+# define ELEMENT_H
+
+# include "structs.h"
+
+Element *createElement();
+void	deleteElement(Element *element);
+void	printElement(Element *element);
+
+#endif
