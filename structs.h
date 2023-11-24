9dddf1136ed03af60e810214255e4e83d20517e2
Luiz-Pastor <luigpacr5@gmail.com>
2023-11-24 10:20:52 +0100

    Added the new database struct

--- structs.h
+++ structs.h
@@ -7,18 +7,23 @@
 # define ISBN_LENGTH 16
 # define MAX_LENGTH 128
 
-typedef struct{
+typedef struct {
     int         key;    /* Book bookID */
     long int    offset; /*Book is stored in disk at this position */
     size_t      size;   /* Book record size. This is a redundant field that helps in the implementation */
 } indexbook;
 
 
-typedef struct{
+typedef struct {
     indexbook   index;
     char        isbn[ISBN_LENGTH + 1];
     char        *title;        /* Tamaño variable, máximo de `MAX_LENGTH`*/
     char        *printedBy;    /* Tamaño variable, máximo de `MAX_LENGTH`*/
 } Element;
+
+typedef struct {
+    int size;
+    Element **elements;
+} Database;
 
 #endif
